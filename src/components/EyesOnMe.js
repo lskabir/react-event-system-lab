87a08fdccaac9a65e861eff9efbf13e8a71be7d2
github username <github email address>
2021-07-22 18:53:40 -0400

    commit tree 2a2835a131d339d1fbe67fcdfeabed9abe50af89

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    focus = () => {
+        console.log('Good!')
+    }
+
+    blur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button onBlur={this.blur} onFocus={this.focus}>Click me!</button>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+
+    typePassword = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <input onKeyUp={this.typePassword} type='password' placeholder='Enter password'/>
+        )
+    }
+}
+
+export default Keypad
